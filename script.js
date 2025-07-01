a4345f1bb0956afe62b87b392cd5ea97301875d7
Santos, Adler <adler.santos@experian.com>
2025-07-01 09:41:43 -0300

    adiciona novo botão e funcionalidade de clique no script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('btn').addEventListener('click', function() {
     document.getElementById('mensagem').textContent = 'Você clicou no botão!';
 });
+
+document.getElementById('btn2').addEventListener('click', function() {
+    document.getElementById('mensagem').textContent = 'Você clicou no novo botão!';
+});
